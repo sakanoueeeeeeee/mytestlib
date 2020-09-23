d68aced63ad9cfb164a58ab36c69725f83346910
sakanoueeeeeeee <71742661+sakanoueeeeeeee@users.noreply.github.com>
2020-09-23 11:14:05 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-saka',
+    version='1.0.0',
+    url='https://pypi.org/manage/account/',
+    author='sakanoue',
+    author_email='h-sakanoue@senjusoft.co.jp',
+    description='My Test Library.',
+    packages=find_packages()
+)
